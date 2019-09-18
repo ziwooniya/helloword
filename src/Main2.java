51bfbfc3b0664c3c735b2da5377c4d6b8cd5403e
ziwooniya <1138330518@qq.com>
2019-09-18 13:59:46 +0800

    1

---
+++ src/Main2.java
@@ -0,0 +1,17 @@
+public class Main2 {
+    private String a;
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("123");
+    }
+}
